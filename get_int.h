7076b06ec2d87c03a94ae7f9e51bedcd06de89b5
Christopher Almquist <somethings90@gmail.com>
2013-04-08 22:44:53 -0600

    Create get_int.h

---
+++ get_int.h
@@ -0,0 +1,14 @@
+#ifndef GET_INT_H
+#define GET_INT_H
+
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cctype>
+
+// Returns an integer if the user inputs a number.
+// Returns INT_MIN if the user inputs something other than a number.
+int get_int();
+
+#endif // GET_INT_H
